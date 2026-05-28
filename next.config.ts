6f08b13b32b369dc069bce3d4db4eae1354ef193
Radya <pratama082004@@gmail.com>
2026-05-28 14:18:49 +0700

    Fix ultimate canvas fallback untuk Vercel SSR build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,25 @@
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   webpack: (config: any) => {
+    // 1. Abaikan canvas di level alias
     config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    
+    // 2. Abaikan canvas di level fallback (Solusi Error SSR Vercel)
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false,
+      fs: false,
+      path: false,
+    };
+    
     return config;
   },
-  // Tambahan agar Turbopack di Next.js 16 tidak ngambek
-  turbopack: {},
 };
 
 export default nextConfig;
